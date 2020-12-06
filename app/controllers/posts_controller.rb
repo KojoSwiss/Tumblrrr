4fcd44e7422bcad22fcc3b2520254eebd6277139
Benjamin Dzandu <kojo.atieku@gmail.com>
2020-12-06 12:13:44 +0100

    Add post

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,33 @@
+class PostsController < ApplicationController
+  # before_action :set_post, :only [:show]
+  def index
+    @posts = Post.all.order('created_at DESC')
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to @path
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
+
+  # def set_post
+  #   @post = Post.find(params[:id])
+  # end
+end
